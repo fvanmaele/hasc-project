b1c8d7823fccb169e22fe5637012578a6252b07f
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2021-07-09 19:55:58 +0200

    sequential algorithm (WIP)

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "stencil.h"
+
+int main() {
+  return 0;
+}
---
+++ simd_selector.h
@@ -0,0 +1,38 @@
+#ifndef SIMDSELECTOR_H
+#define SIMDSELECTOR_H
+#include <cstddef>
+#include <vectorclass.h>
+
+namespace hasc
+{
+template<size_t simd_width>
+struct SIMDSelector
+{};
+
+template<>
+struct SIMDSelector<2>
+{
+  static const size_t simd_width = 2;
+  static const size_t simd_registers = 16;
+  typedef Vec2d SIMDType;
+};
+
+template<>
+struct SIMDSelector<4>
+{
+  static const size_t simd_width = 4;
+  static const size_t simd_registers = 16;
+  typedef Vec4d SIMDType;
+};
+
+template<>
+struct SIMDSelector<8>
+{
+  static const size_t simd_width = 8;
+  static const size_t simd_registers = 32;
+  typedef Vec8d SIMDType;
+};
+
+} // namespace hasc
+
+#endif // SIMDSELECTOR_H
